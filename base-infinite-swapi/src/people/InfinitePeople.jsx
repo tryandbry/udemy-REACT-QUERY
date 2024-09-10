64f1a2e36d2ae311f97f0a85badff5d632cc5f2e
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-09-10 15:45:09 -0500

    infinite scroll for people and species

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,65 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Person } from "./Person";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const baseUrl = "https://swapi-node.vercel.app";
+const initialUrl = `${baseUrl}/api/people/`;
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next ? `${baseUrl}${lastPage.next}` : undefined;
+    },
+  })
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>
+  }
+
+  if (isError) {
+    return <div className="error">Error: {error.toString()}</div>
+  }
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll
+        loadMore={() => {
+          if (!isFetching) {
+            fetchNextPage()
+          }
+        }}
+        hasMore={hasNextPage}
+        initialLoad={false}
+      >;
+        {data.pages.map((pageData) => {
+          return pageData.results.map(
+            (person) => (
+              <Person
+                key={person.fields.name}
+                name={person.fields.name}
+                hairColor={person.fields.hair_color}
+                eyeColor={person.fields.eye_color}
+              />
+            )
+          )
+        })}
+      </InfiniteScroll>
+    </>
+  )
 }
