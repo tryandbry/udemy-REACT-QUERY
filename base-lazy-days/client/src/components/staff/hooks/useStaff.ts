a0fd86fb4bf76b916621c6cd33fcf57763277d0a
Bryan W. Kao <bryan.w.kao@gmail.com>
2024-09-11 11:57:28 -0500

    section 4 done

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import type { Staff } from "@shared/types";
 
@@ -6,19 +7,22 @@ import { filterByTreatment } from "../utils";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { GiCrescentStaff } from "react-icons/gi";
 
-// query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 export function useStaff() {
+  const fallback: Staff[] = [];
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
+  const { data: staff = fallback } = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  })
 
   return { staff, filter, setFilter };
 }
